0c9610be11f4dbb0d9ef03c4685787d9a0752ea8
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-11-22 15:16:33 +0100

    Changes nav menu layout and removes login button if user data is stored, displays user info in nav if logged in

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -6,37 +6,52 @@ import { DEFAULT_URLS, URLS } from "./constants.js";
 export async function renderNav() {
   const { pathname } = document.location;
 
-  const navbarTop = document.querySelector(".navbar-top");
-  const nav = document.querySelector(".nav-rendered");
+  const userInfoHeader = document.querySelector(".user-info-header");
+  const navLinks = document.querySelector(".nav-links");
+  const navButtons = document.querySelector(".nav-buttons");
 
-  const userUrl = `${API_PATH}/auction/profiles/${getItem(
-    "name"
-  )}?_listings=true`;
+  const links = [
+    { href: URLS.INDEX, text: "Home" },
+    { href: URLS.PROFILE, text: "Profile" },
+  ];
+
+  navButtons.innerHTML = `
+  <div class="nav-divider"></div>
+  <li class="nav-item nav-regular">
+      <button class="nav-link text-white btn-login" data-bs-toggle="modal"
+      data-bs-target="#loginModal"><span>Log in</span></button>
+  </li>`;
 
   try {
-    const response = await fetch(`${userUrl}`, { headers: headers() });
-    const apiUserData = await response.json();
+    if (getItem("name")) {
+      const userUrl = `${API_PATH}/auction/profiles/${getItem(
+        "name"
+      )}?_listings=true`;
 
-    const links = [
-      { href: URLS.INDEX, text: "Home" },
-      { href: URLS.PROFILE, text: "Profile" },
-    ];
+      const response = await fetch(`${userUrl}`, { headers: headers() });
+      const apiUserData = await response.json();
 
-    const userInfoHeader = document.querySelector(".user-info-header");
-
-    if (apiUserData) {
-      if (apiUserData.avatar === null) {
-        apiUserData.avatar = DEFAULT_URLS.AVATAR;
+      if (apiUserData) {
+        navButtons.innerHTML = "";
       }
 
-      userInfoHeader.innerHTML = `
-        <div>
-            <img src="${apiUserData.avatar}" class="avatar" alt="${apiUserData.name}'s avatar" onerror='this.src="${DEFAULT_URLS.AVATAR}"' width="40">
-            <a href="${URLS.PROFILE}?name=${apiUserData.name}">${apiUserData.name}</a>
-        </div>`;
+      if (response.ok) {
+        if (apiUserData.avatar === null) {
+          apiUserData.avatar = DEFAULT_URLS.AVATAR;
+        }
+
+        userInfoHeader.innerHTML = `
+            <a href="${URLS.PROFILE}?name=${apiUserData.name}" class="d-flex gap-2 text-decoration-none text-dark">
+                <img src="${apiUserData.avatar}" class="avatar" alt="${apiUserData.name}'s avatar" onerror='this.src="${DEFAULT_URLS.AVATAR}"'>
+                <div class="d-flex flex-column">
+                    <p class="mb-0 fw-medium">${apiUserData.name}</p>
+                    <p>Credit(s): ${apiUserData.credits}</p>
+                </div>
+            </a>`;
+      }
     }
 
-    nav.innerHTML = links
+    navLinks.innerHTML = links
       .map(
         (link) => `
     <li class="nav-item nav-regular">
