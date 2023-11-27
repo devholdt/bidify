9b08c4c2e818ecea3a74b2628eed89f86ea2e43e
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-11-27 17:48:36 +0100

    Adds possibility of adding multiple tags in same fashion of the media URL input(s)

--- src/js/api/listings/create.js
+++ src/js/api/listings/create.js
@@ -6,6 +6,7 @@ export async function createListing() {
   const tagsInput = document.querySelector("#createListingTags");
 
   handleMediaInputs();
+  handleTagInputs();
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -13,12 +14,14 @@ export async function createListing() {
     const title = titleInput.value;
     const endDate = endDateInput.value;
     const description = descriptionInput.value;
-    const tags = tagsInput.value.split(", ").map((tag) => tag.trim());
 
     const mediaInputs = document.querySelectorAll(
       "#mediaInputsContainer input"
     );
     const media = Array.from(mediaInputs).map((input) => input.value);
+
+    const tagInputs = document.querySelectorAll("#tagInputsContainer input");
+    const tags = Array.from(tagInputs).map((input) => input.value);
 
     console.log("Title: ", title);
     console.log("End date: ", endDate);
@@ -32,7 +35,7 @@ function handleMediaInputs() {
   document.getElementById("addMediaButton").addEventListener("click", () => {
     const container = document.getElementById("mediaInputsContainer");
     const newInput = document.createElement("div");
-    newInput.setAttribute("class", "input-group mb-2");
+    newInput.classList.add("input-group", "mb-2");
     newInput.innerHTML = `
         <input type="text" class="form-control" placeholder="media URL">
 
@@ -54,6 +57,32 @@ function handleMediaInputs() {
     const removeMediaButton = newInput.querySelector(".remove-media");
 
     removeMediaButton.addEventListener("click", (event) => {
+      container.removeChild(event.target.parentElement.parentElement);
+    });
+  });
+}
+
+function handleTagInputs() {
+  document.getElementById("addTagButton").addEventListener("click", () => {
+    const container = document.getElementById("tagInputsContainer");
+    const newInput = document.createElement("div");
+    newInput.classList.add("input-group", "mb-2");
+    newInput.innerHTML = `
+        <input type="text" class="form-control rounded-end" placeholder="tag">
+
+        <button type="button"
+            class="btn border-0 input-group-text text-danger remove-tag">
+            <span class="material-icons">remove</span>
+        </button>`;
+
+    if (container.children.length === 4) {
+      return;
+    } else {
+      container.appendChild(newInput);
+    }
+
+    const removeTagButton = newInput.querySelector(".remove-tag");
+    removeTagButton.addEventListener("click", (event) => {
       container.removeChild(event.target.parentElement.parentElement);
     });
   });
