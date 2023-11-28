c47e7bac9694294f6ccfc907462c916733915c6b
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-11-28 12:30:32 +0100

    Modifies listing sorting to be accurate

--- src/js/api/listings/latestListings.js
+++ src/js/api/listings/latestListings.js
@@ -1,12 +1,10 @@
 import { getListings } from "./index.js";
-import { sortListings } from "../../utilities/index.js";
 import { createCard } from "../../components/index.js";
 
 export async function latestListings() {
-  const listings = await getListings();
-  const sortedListings = sortListings(listings, "created");
+  const listings = await getListings({ sort: "&sort=created" });
 
-  sortedListings
+  listings
     .slice(0, 3)
     .forEach((listing) => createCard(listing, ".latest-listings"));
 }
--- src/js/components/createListingForm.js
+++ src/js/components/createListingForm.js
@@ -6,9 +6,18 @@ export const createListingForm = () => {
     .querySelector("form#createListingForm")
     .addEventListener("submit", createListingEvent);
 
+  // Handles adding and removing additional media and tag inputs
   handleInputs("mediaInputsContainer", "Media", "media URL", true);
   handleInputs("tagInputsContainer", "Tag", "tag");
 
+  // Sets the default date input value to local date + one day
+  const todayDate = new Date();
+  const tomorrowDate = todayDate.setDate(todayDate.getDate() + 1);
+  document.getElementById("createListingDate").valueAsDate = new Date(
+    tomorrowDate
+  );
+
+  // Button to clear the media URL input(s)
   const clearButton = document.querySelector(".clear-button");
   clearButton.addEventListener("click", () => {
     document.querySelector("input[name='media']").value = "";
