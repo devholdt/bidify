095a9fcc691fd9090c97db18dc17f72bab7a6d71
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-12-07 15:16:57 +0100

    Optimizes the javascript in navigation.js and setupNav.js

--- src/js/utilities/setupNav.js
+++ src/js/utilities/setupNav.js
@@ -1,27 +1,34 @@
 import { URLS, DEFAULT_URLS } from "../components/index.js";
-import { getItem } from "../storage/index.js";
+import { getItem, getUser } from "../storage/index.js";
 
-export function setupNav(
-  navButtons,
-  bannerButtons,
-  navLinks,
-  navLinksCollapse,
-  links
-) {
+export function setupNav(elements, links) {
+  const isMobileView = window.innerWidth < 992;
   const isLoggedIn = getItem("name");
-  updateNavButtons(isLoggedIn, navButtons, bannerButtons);
-  updateNavLinks(navLinks, links);
 
-  if (window.innerWidth < 992) {
-    updateNavLinks(navLinksCollapse, links);
+  updateNavButtons(isLoggedIn, elements.navButtons, elements.bannerButtons);
+  updateNavLinks(elements.navLinks, links, isMobileView);
+  updateNavLinksCollapse(elements.navLinksCollapse, links, isMobileView);
+
+  if (isLoggedIn) {
+    const userData = getUser();
+    updateUserInfo(getItem("name"), elements, userData, isMobileView);
   } else {
+    elements.navLinksCollapse.classList.remove("border-top", "pt-3");
+  }
+}
+
+export function updateNavLinksCollapse(navLinksCollapse, links, isMobileView) {
+  if (isMobileView) {
+    updateNavLinks(navLinksCollapse, links);
+    navLinksCollapse.classList.add("border-top", "pt-3");
+  } else {
+    navLinksCollapse.classList.remove("border-top", "pt-3");
     navLinksCollapse.innerHTML = "";
   }
 }
 
 export function updateNavLinks(container, links) {
   const { pathname } = document.location;
-
   container.innerHTML = links
     .map(
       (link) => `
@@ -36,21 +43,14 @@ export function updateNavLinks(container, links) {
     .join("");
 }
 
-export function updateUserInfo(
-  isLoggedIn,
-  headerContainer,
-  collapseContainer,
-  userData
-) {
-  if (isLoggedIn) {
-    collapseContainer.innerHTML = "";
-    userInfo(headerContainer, userData);
+export function updateUserInfo(userName, elements, userData, isMobileView) {
+  if (userName) {
+    userInfo(elements.userInfoHeader, userData);
 
-    if (screen.width < 992) {
-      collapseContainer.innerHTML = "";
-      userInfo(collapseContainer, userData);
+    if (isMobileView) {
+      userInfo(elements.userInfoCollapse, userData);
     } else {
-      collapseContainer.innerHTML = "";
+      elements.userInfoCollapse.innerHTML = "";
     }
   }
 }
@@ -58,15 +58,18 @@ export function updateUserInfo(
 function userInfo(container, userData) {
   container.innerHTML = `
       <div class="d-flex">
-        <a href="${URLS.PROFILE}?name=${userData.name}" class="d-flex gap-2 text-decoration-none text-dark pe-2">
-          <img src="${userData.avatar}" class="avatar" alt="${userData.name}'s avatar" onerror='this.src="${DEFAULT_URLS.AVATAR}"'>
+        <a href="${URLS.PROFILE}?name=${
+    userData.name
+  }" class="d-flex gap-2 text-decoration-none text-dark pe-2">
+          <img src="${
+            userData.avatar || DEFAULT_URLS.AVATAR
+          }" class="avatar" alt="${userData.name}'s avatar">
           <div class="d-flex flex-column">
             <p class="mb-0 fw-light">${userData.name}</p>
             <p class="text-primary fw-normal">$${userData.credits}</p>
           </div>
         </a>
-        <button class="btn btn-outline-dark h-100 my-auto ms-2 rounded-0 shadow-sm" 
-        data-bs-toggle="modal" data-bs-target="#logoutModal">
+        <button class="btn btn-outline-dark h-100 my-auto ms-2 rounded-0 shadow-sm" data-bs-toggle="modal" data-bs-target="#logoutModal">
           <span>logout</span>
         </button>
       </div>`;
@@ -77,7 +80,7 @@ function updateNavButtons(isLoggedIn, navButton, bannerButton) {
     navButton.innerHTML = `
         <div class="nav-divider"></div>
         <li class="nav-item nav-regular">
-            <button class="nav-link text-white btn-login" 
+            <button class="nav-link text-white btn-login"
             data-bs-toggle="modal" data-bs-target="#logoutModal">
               <span>Logout</span>
             </button>
@@ -97,7 +100,7 @@ function updateNavButtons(isLoggedIn, navButton, bannerButton) {
 
     if (bannerButton) {
       bannerButton.innerHTML = `
-        <button type="button" class="btn btn-primary btn-cta" 
+        <button type="button" class="btn btn-primary btn-cta"
         data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>`;
     }
   }
