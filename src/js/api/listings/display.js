b1e3c096fe70874f05752328517046f88fa8be62
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-12-08 19:26:59 +0100

    Adds sort options for bid amount (high-low and low-high) and ends soon

--- src/js/api/listings/display.js
+++ src/js/api/listings/display.js
@@ -12,31 +12,68 @@ export async function displayListings() {
   const sortListingsContainer = document.querySelector("#sortListings");
   const listingsContainer = document.querySelector(".listings");
 
-  let allListings = await getListings({ sort: "&sort=created" });
+  let allListingsByCreated = await getListings({ sort: "&sort=created" });
+  let allListingsByEndsAt = await getListings({
+    sortOrder: "asc",
+    sort: "&sort=endsAt",
+  });
 
-  display(listingsContainer, allListings.slice(0, 12));
+  display(listingsContainer, allListingsByCreated.slice(0, 12));
 
-  sortListingsContainer.addEventListener("change", (event) => {
+  sortListingsContainer.addEventListener("change", async (event) => {
     let sortedListings;
 
     switch (event.target.value) {
       case "Latest":
-        sortedListings = allListings.slice();
+        sortedListings = allListingsByCreated;
         break;
       case "Popular":
-        sortedListings = [...allListings].sort(
+        sortedListings = [...allListingsByCreated].sort(
           (a, b) => b._count.bids - a._count.bids
         );
         break;
-      case "Title A - Z":
-        sortedListings = [...allListings].sort((a, b) =>
+      case "Title A-Z":
+        sortedListings = [...allListingsByCreated].sort((a, b) =>
           a.title.localeCompare(b.title)
         );
         break;
-      case "Title Z - A":
-        sortedListings = [...allListings].sort((a, b) =>
+      case "Title Z-A":
+        sortedListings = [...allListingsByCreated].sort((a, b) =>
           b.title.localeCompare(a.title)
         );
+        break;
+      case "Bid high-low":
+        sortedListings = [...allListingsByCreated].sort((a, b) => {
+          let maxBidA = a.bids.reduce(
+            (max, bid) => (bid.amount > max ? bid.amount : max),
+            0
+          );
+          let maxBidB = b.bids.reduce(
+            (max, bid) => (bid.amount > max ? bid.amount : max),
+            0
+          );
+
+          return maxBidB - maxBidA;
+        });
+        break;
+      case "Bid low-high":
+        sortedListings = [...allListingsByCreated]
+          .filter((listing) => listing.bids.length > 0)
+          .sort((a, b) => {
+            let maxBidA = a.bids.reduce(
+              (max, bid) => (bid.amount > max ? bid.amount : max),
+              0
+            );
+            let maxBidB = b.bids.reduce(
+              (max, bid) => (bid.amount > max ? bid.amount : max),
+              0
+            );
+
+            return maxBidA - maxBidB;
+          });
+        break;
+      case "Ends soon":
+        sortedListings = allListingsByEndsAt;
         break;
       default:
         sortedListings = allListings.slice();
