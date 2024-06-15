a6a6d45118f660015f379673f571c5b88039e3c4
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-15 17:09:53 +0200

    changes to how listings and profile listings are fetched

--- src/js/api/listings/display.js
+++ src/js/api/listings/display.js
@@ -8,23 +8,23 @@ import { alert } from "../../utilities/index.js";
  * @param {Array} listings - An array of listing objects to be displayed.
  */
 function display(listings) {
-  const listingsContainer = document.querySelector(".listings");
-  const button = document.querySelector("#buttonMoreResults");
-  const searchResults = document.querySelector(".search-results");
-  const searchInput = document.querySelector("#searchListings");
-  listingsContainer.innerHTML = "";
-  listingsContainer.style.display = "flex";
-  searchResults.innerHTML = "";
-  button.style.display = "flex";
-  searchInput.value = "";
+	const listingsContainer = document.querySelector(".listings");
+	const button = document.querySelector("#buttonMoreResults");
+	const searchResults = document.querySelector(".search-results");
+	const searchInput = document.querySelector("#searchListings");
+	listingsContainer.innerHTML = "";
+	listingsContainer.style.display = "flex";
+	searchResults.innerHTML = "";
+	button.style.display = "flex";
+	searchInput.value = "";
 
-  listings
-    .map((listing) => {
-      return listing.title === ""
-        ? { ...listing, title: "[listing]" }
-        : listing;
-    })
-    .forEach((listing) => createCard(listing, ".listings"));
+	listings
+		.map((listing) => {
+			return listing.title === ""
+				? { ...listing, title: "[listing]" }
+				: listing;
+		})
+		.forEach((listing) => createCard(listing, ".listings"));
 }
 
 /**
@@ -34,122 +34,122 @@ function display(listings) {
  * @throws {Error} - Throws and error if fetching listings fails.
  */
 export async function displayListings() {
-  const sortListingsContainer = document.querySelector("#sortListings");
-  const listingsContainer = document.querySelector(".listings");
-  const button = document.querySelector("#buttonMoreResults");
-  const spanResults = document.querySelector(".span-results");
+	const sortListingsContainer = document.querySelector("#sortListings");
+	const listingsContainer = document.querySelector(".listings");
+	const button = document.querySelector("#buttonMoreResults");
+	const spanResults = document.querySelector(".span-results");
 
-  listingsContainer.innerHTML = "";
+	listingsContainer.innerHTML = "";
 
-  const INITIAL_LIMIT = 12;
-  let limit = INITIAL_LIMIT;
-  let currentSort = "Latest";
-  let allListings = [];
+	const INITIAL_LIMIT = 12;
+	let limit = INITIAL_LIMIT;
+	let currentSort = "Latest";
+	let allListings = [];
 
-  for (let i = 0; i < INITIAL_LIMIT; i++) {
-    listingsContainer.innerHTML += placeholderCard;
-  }
+	for (let i = 0; i < INITIAL_LIMIT; i++) {
+		listingsContainer.innerHTML += placeholderCard;
+	}
 
-  async function fetchAllListings() {
-    try {
-      allListings = await getListings({ sort: "&sort=created" });
-    } catch (error) {
-      alert(
-        "danger",
-        "An error occured when attempting to fetch all listings",
-        ".alert-absolute",
-        null
-      );
-      throw new Error(
-        `An error occured when attempting to fetch all listings: ${error}`
-      );
-    }
-  }
+	async function fetchAllListings() {
+		try {
+			allListings = await getListings({ sort: "&sort=created" });
+		} catch (error) {
+			alert(
+				"danger",
+				"An error occured when attempting to fetch all listings",
+				".alert-absolute",
+				null
+			);
+			throw new Error(
+				`An error occured when attempting to fetch all listings: ${error}`
+			);
+		}
+	}
 
-  function sortAndDisplayListings() {
-    let sortedListings = [...allListings];
+	function sortAndDisplayListings() {
+		let sortedListings = [...allListings];
 
-    switch (currentSort) {
-      case "Latest":
-        sortedListings = [...allListings].sort(
-          (a, b) => new Date(b.created) - new Date(a.created)
-        );
-        break;
-      case "Popular":
-        sortedListings = [...allListings].sort(
-          (a, b) => b._count.bids - a._count.bids
-        );
-        break;
-      case "Title A-Z":
-        sortedListings = [...allListings].sort((a, b) =>
-          a.title.localeCompare(b.title)
-        );
-        break;
-      case "Title Z-A":
-        sortedListings = [...allListings].sort((a, b) =>
-          b.title.localeCompare(a.title)
-        );
-        break;
-      case "Bid high-low":
-        sortedListings = [...allListings].sort((a, b) => {
-          let maxBidA = a.bids.reduce(
-            (max, bid) => (bid.amount > max ? bid.amount : max),
-            0
-          );
-          let maxBidB = b.bids.reduce(
-            (max, bid) => (bid.amount > max ? bid.amount : max),
-            0
-          );
+		switch (currentSort) {
+			case "Latest":
+				sortedListings = [...allListings].sort(
+					(a, b) => new Date(b.created) - new Date(a.created)
+				);
+				break;
+			case "Popular":
+				sortedListings = [...allListings].sort(
+					(a, b) => b._count.bids - a._count.bids
+				);
+				break;
+			case "Title A-Z":
+				sortedListings = [...allListings].sort((a, b) =>
+					a.title.localeCompare(b.title)
+				);
+				break;
+			case "Title Z-A":
+				sortedListings = [...allListings].sort((a, b) =>
+					b.title.localeCompare(a.title)
+				);
+				break;
+			case "Bid high-low":
+				sortedListings = [...allListings].sort((a, b) => {
+					let maxBidA = a.bids.reduce(
+						(max, bid) => (bid.amount > max ? bid.amount : max),
+						0
+					);
+					let maxBidB = b.bids.reduce(
+						(max, bid) => (bid.amount > max ? bid.amount : max),
+						0
+					);
 
-          return maxBidB - maxBidA;
-        });
-        break;
-      case "Bid low-high":
-        sortedListings = [...allListings]
-          .filter((listing) => listing.bids.length > 0)
-          .sort((a, b) => {
-            let maxBidA = a.bids.reduce(
-              (max, bid) => (bid.amount > max ? bid.amount : max),
-              0
-            );
-            let maxBidB = b.bids.reduce(
-              (max, bid) => (bid.amount > max ? bid.amount : max),
-              0
-            );
+					return maxBidB - maxBidA;
+				});
+				break;
+			case "Bid low-high":
+				sortedListings = [...allListings]
+					.filter((listing) => listing.bids.length > 0)
+					.sort((a, b) => {
+						let maxBidA = a.bids.reduce(
+							(max, bid) => (bid.amount > max ? bid.amount : max),
+							0
+						);
+						let maxBidB = b.bids.reduce(
+							(max, bid) => (bid.amount > max ? bid.amount : max),
+							0
+						);
 
-            return maxBidA - maxBidB;
-          });
-        break;
-      case "Ends soon":
-        sortedListings = [...allListings].sort(
-          (a, b) => new Date(a.endsAt) - new Date(b.endsAt)
-        );
-        break;
-    }
+						return maxBidA - maxBidB;
+					});
+				break;
+			case "Ends soon":
+				sortedListings = [...allListings].sort(
+					(a, b) => new Date(a.endsAt) - new Date(b.endsAt)
+				);
+				break;
+		}
 
-    display(sortedListings.slice(0, limit));
-  }
+		display(sortedListings.slice(0, limit));
+	}
 
-  await fetchAllListings();
-  sortAndDisplayListings();
+	await fetchAllListings();
+	sortAndDisplayListings();
 
-  spanResults.innerHTML = `(${limit})`;
+	spanResults.innerHTML = `(${limit})`;
 
-  sortListingsContainer.addEventListener("change", (event) => {
-    limit = INITIAL_LIMIT;
-    spanResults.innerHTML = `(${limit})`;
-    currentSort = event.target.value;
-    sortAndDisplayListings();
-  });
+	sortListingsContainer.addEventListener("change", (event) => {
+		limit = INITIAL_LIMIT;
+		spanResults.innerHTML = `(${limit})`;
+		currentSort = event.target.value;
+		sortAndDisplayListings();
+	});
 
-  button.addEventListener("click", () => {
-    limit += INITIAL_LIMIT;
-    sortAndDisplayListings();
+	button.addEventListener("click", () => {
+		limit += INITIAL_LIMIT;
+		sortAndDisplayListings();
 
-    spanResults.innerHTML = `(${limit})`;
+		spanResults.innerHTML = `(${limit})`;
 
-    if (limit >= allListings.length) {
-      button.style.display = "none";
-    }
-  });
+		if (limit >= allListings.length) {
+			button.style.display = "none";
+		}
+	});
 }
--- src/js/api/profiles/profileListings.js
+++ src/js/api/profiles/profileListings.js
@@ -2,11 +2,12 @@ import { getProfile, getProfileListings } from "./index.js";
 import { getItem } from "../../storage/index.js";
 import {
 	createCard,
-	createBidCard,
+	createBidRow,
 	createWinCard,
 } from "../../components/index.js";
 import { alert } from "../../utilities/index.js";
 import { getSingleListing } from "../listings/index.js";
+import { checkboxState } from "../../components/checkboxState.js";
 
 /**
  * Fetches and displays the listings associated with the current user's profile.
@@ -47,17 +48,25 @@ export async function profileListings() {
 /**
  * Fetches and displays the bids made by the current user.
  *
+ * @return {Promise<void>} Resolves when the bids are fetched and displayed.
  * @throws {Error} Throws an error if fetching profile bids fails.
  */
 export async function profileBids() {
 	try {
 		if (getItem("name")) {
 			const bids = await getProfile(getItem("name"), true);
+
 			const profileBidsContainer = document.querySelector(".profile-bids");
 
 			if (bids.length > 0) {
-				// profileBidsContainer.classList.add("row", "row-cols-3");
-				bids.forEach((bid) => createBidCard(bid, ".profile-bids"));
+				bids.forEach((bid) => {
+					if (bid && bid.listing) {
+						createBidRow(bid, ".profile-bids");
+					} else {
+						console.error("Invalid bid or listing data:", bid);
+					}
+				});
+				checkboxState("Bids", profileBidsContainer);
 			} else {
 				profileBidsContainer.innerHTML = `<p class="d-flex justify-content-center">You have no active bids</p>`;
 				document.querySelector(".toggle-active-bids").style.display = "none";
@@ -66,13 +75,13 @@ export async function profileBids() {
 	} catch (error) {
 		alert(
 			"danger",
-			"An error occured when attempting to get profile bids. <strong>This does not mean your bids are inactive.</strong>",
+			"An error occurred when attempting to get profile bids. <strong>This does not mean your bids are inactive.</strong>",
 			".profile-bids",
 			null,
 			false
 		);
 		throw new Error(
-			`An error occured when attempting to get profile bids: ${error}`
+			`An error occurred when attempting to get profile bids: ${error}`
 		);
 	}
 }
